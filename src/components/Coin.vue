f2b0727c26e1ff0391a63a8274be0e2992d52616
Valentin Bailleul <vlt.bailleul@gmail.com>
2017-02-26 14:15:12 +0100

    create coin component

---
+++ src/components/Coin.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="coin"></div>
+</template>
+
+<script>
+
+  export default {
+
+    name: 'coin'
+  }
+
+</script>
+
+<style lang="scss" scoped>
+
+  .coin {
+      width: 80%;
+      height: 70%;
+      border-radius: 50%;
+      transition: all 1000ms ease;
+      position: relative;
+      box-shadow: 0px 2px 4px rgba(0,0,0,0.75);
+      margin-bottom: 2px;
+      border-bottom-width: 2px;
+      border-bottom-style: solid;
+      opacity: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      &.black, &.white {
+        opacity: 1;
+        &::after {
+          content: '';
+          width: 75%;
+          height: 75%;
+          border-radius: 50%;
+          opacity: 0.10;
+        }
+      }
+
+      &.black {
+        background-color: black; 
+        border-bottom-color: white;
+
+        &::after {
+          background: linear-gradient(transparent, white);
+        }
+      }
+
+      &.white {
+        background-color: white; 
+        border-bottom-color: black;
+        
+        &::after {
+          background: linear-gradient(black, transparent);
+        }
+      }
+
+      &.hint {
+        transition: opacity 100ms ease;
+        opacity: 0.1;
+
+        &:hover {
+          opacity: 0.5; 
+        }
+      }
+  }
+
+</style>
--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="tile">
-    <div class="coin" :class="color + (hint ? ' hint' : '')"></div>
+    <coin v-if="state || hint" :class="hint ? 'hint ' + currentColor : color"></coin>
   </div>
 </template>
 
 <script>
 
   import { COLORS } from '../constants/CoinStates'
+  import Coin from './Coin'
+  import { mapGetters } from 'vuex'
 
   export default {
 
@@ -37,9 +39,17 @@
     },
 
     computed: {
+      ...mapGetters([
+        'currentColor'
+      ]),
+
       color: function () {
         return COLORS[this.state]
       }
+    },
+
+    components: {
+      Coin
     }
   }
 
@@ -54,65 +64,6 @@
     justify-content: center;
     align-items: center;
     position: relative;
-
-    &:hover::before, .coin.hint {
-        content: '';
-        position: absolute;
-        width: 80%;
-        height: 70%;
-        border-radius: 50%;
-        transition: all 100ms ease;
-        opacity: 0.10;
-        background-color: white;
-      }
-
-
-    .coin {
-        width: 80%;
-        height: 70%;
-        border-radius: 50%;
-        transition: all 500ms ease;
-        position: relative;
-        box-shadow: 0px 2px 4px rgba(0,0,0,0.75);
-        margin-bottom: 2px;
-        border-bottom-width: 2px;
-        border-bottom-style: solid;
-        opacity: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        &.black, &.white {
-          opacity: 1;
-          &::after {
-            content: '';
-            width: 75%;
-            height: 75%;
-            border-radius: 50%;
-            opacity: 0.10;
-          }
-        }
-
-
-        &.black {
-          background-color: black; 
-          border-bottom-color: white;
-
-          &::after {
-            background: linear-gradient(transparent, white);
-          }
-        }
-
-        &.white {
-          background-color: white; 
-          border-bottom-color: black;
-          
-          &::after {
-            background: linear-gradient(black, transparent);
-          }
-        }
-
-    }
 }
 
 </style>
